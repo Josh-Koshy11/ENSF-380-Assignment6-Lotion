c16b1be0b7e68a50862eea3aea12a58917fd1880
Joshua Koshy <joshua.koshy@ucalgary.ca>
2023-03-09 22:48:03 -0700

    full web layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-  <>
-  <h1>Lotion</h1>
-  
+    <>
+    <header>
+      <h1>Lotion</h1>
+      <p id="subh"> Like Notion, but worse. </p>
+    </header>
+    <button type="button" id="lefttoggle">&#9776;</button>
+    <div className="contentpage">
+      <div id="sidebar">
+        <div id="sidetext">
+          <p>Note</p>
+          <button type="button" id="newnote">&#43;</button>
+        </div>
+        <div id="buttoncontainer">
+          <button type="button" id="1">untitled</button>
+        </div>
+      </div>
+      <div id="content">
+        <p id="refprompt">Select a note, or create a new one.</p>
+      </div>
+    </div>
   </>
   )
 };
